3df217a81b889d89e62d6471b9a4f70f6dbc5609
wojga476 <wojga476@gmail.com>
2018-06-05 15:27:50 +0200

    Zadanie 1-zajecia4, sprawdzenie czy trójkąt prostokątny Zadanie 2 quiz matematyczny

---
+++ src/MathQuiz.java
@@ -0,0 +1,63 @@
+
+public class MathQuiz {
+
+    private double wynMnoz;
+    private double wynPola;
+    private double wynKwad;
+//
+//    public MathQuiz(double wynMnoz, double wynPola, double wynKwad) {
+//        this.wynMnoz = wynMnoz;
+//        this.wynPola = wynPola;
+//        this.wynKwad = wynKwad;
+//    }
+//
+//    public void setWynMnoz(double wynMnoz) {
+//        this.wynMnoz = wynMnoz;
+//    }
+//
+//    public void setWynPola(double wynPola) {
+//        this.wynPola = wynPola;
+//    }
+//
+//    public void setWynKwad(double wynKwad) {
+//        this.wynKwad = wynKwad;
+//    }
+//
+//    public double getWynMnoz() {
+//        return wynMnoz;
+//    }
+//
+//    public double getWynPola() {
+//        return wynPola;
+//    }
+//
+//    public double getWynKwad() {
+//        return wynKwad;
+//    }
+
+    public void question1(){
+        if(wynMnoz*wynMnoz ==15) {
+            System.out.println("Prawda");
+        }
+        else{
+            System.out.println("Fałsz");
+        }
+    }
+    public void question2(){
+        if(wynPola ==144) {
+            System.out.println("Prawda");
+        }
+        else{
+            System.out.println("Fałsz");
+        }
+    }
+    public void question3(){
+        if(wynKwad ==228886641) {
+            System.out.println("Prawda");
+        }
+        else{
+            System.out.println("Fałsz");
+        }
+    }
+
+}
---
+++ src/QuizTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class QuizTest {
+    public static void main(String[] args) {
+
+        MathQuiz wyniki = new MathQuiz();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Jaki jest wynik mnożenia 3*5?");
+        int wynMnoz = input.nextInt();
+        System.out.println("Jakie jest pole kwadratu o boku 12?");
+        int wynPola = input.nextInt();
+        System.out.println("Jaki jest pierwiastek kwadratowy liczby 15129?");
+        int wynKwad = input.nextInt();
+        input.close();
+        System.out.println("Wyniki testu:");
+        wyniki.question1();
+        wyniki.question2();
+        wyniki.question3();
+
+
+        }
+
+    }
+
+
